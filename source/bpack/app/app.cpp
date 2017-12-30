06e28afffedb84d4a96aaa135022c1381e1fca6c
blueback <blueback28@gmail.com>
2017-12-31 03:35:10 +0900

    packファイル作成、チェック

--- source/bpack/app/app.cpp
+++ source/bpack/app/app.cpp
@@ -37,10 +37,42 @@ namespace NApp
 		
 		//ファイルシステム起動。
 		NBsys::NFile::StartSystem(1);
-		NBsys::NFile::SetRoot(0,L"./");
+		NBsys::NFile::SetRoot(0,L"../");
 
 		{
-			std::cout << "end" << std::endl;
+			//作成開始。
+			sharedptr<ThreadTemplate<NBsys::NFile::File_Pack_MakeThread>> t_pack_makethread = NBsys::NFile::Pack_Create(L"../to_pack_data/",L"../data.pack",nullptr);
+			
+			std::cout << "pack : start" << std::endl;
+
+			//作成中。
+			while(t_pack_makethread->IsEnd() == false){
+				ThreadSleep(1000);
+				std::cout << "pack : now" << std::endl;
+			}
+			
+			//作成完了。
+			t_pack_makethread->EndWait();
+			t_pack_makethread.reset();
+
+			std::cout << "pack : end" << std::endl;
+		}
+
+		{
+			NBsys::NFile::Pack_LoadRequest(0,L"data.pack",L"test");
+
+			//TODO:エラー処理。
+			while(NBsys::NFile::Pack_IsExist(0,L"data.pack") == false){
+				ThreadSleep(1000);
+				//std::cout << "error : isexist" << std::endl;
+			}
+
+			sharedptr<NBsys::NFile::File_Object> t_object(new NBsys::NFile::File_Object(0,L"test/test1/data.txt",-1,nullptr,0));
+
+			while(t_object->IsBusy()){
+				ThreadSleep(1000);
+			}
+
 		}
 
 		//終了処理。
